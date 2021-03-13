bf933a7edb5590d6a8411b9ed24fb45db43d4421
mlapshin <mlapshin@uwaterloo.ca>
2021-03-13 01:56:11 -0500

    Added 3-point based angle calculator function

--- DataObtainerScript/main.py
+++ DataObtainerScript/main.py
@@ -5,10 +5,34 @@
     - Some mediapipe code was taken from https://google.github.io/mediapipe/solutions/hands for capturing and processing the video feed.
 """
 
+import numpy
 import cv2
 import mediapipe as mp
 
-# def coorToAngle(r1, r2, s):
+
+def normalized_3d_vector(v):
+    return numpy.sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2])
+
+
+def dot_product_3d_vector(a, b):
+    return a[0] * b[0] + a[1] * b[1] + a[2] * b[2]
+
+
+# r1, r2, s are the coordinates of the 3 points.
+# r2 is the point in between the reference point r1 and the point for which we are trying to interpret the angle s
+# @return, angle in radians: angle is bound by [0, pi]
+def coord2angle(r1, r2, s):
+    reference_vector = [r2[0] - r1[0], r2[1] - r1[1], r2[2] - r1[2]]
+    direction_vector = [r1[0] - s[0], r1[1] - s[1], r1[2] - s[2]]
+
+    # Math calculations
+    reference_norm = normalized_3d_vector(reference_vector)
+    direction_norm = normalized_3d_vector(direction_vector)
+    reference_dot_direction = dot_product_3d_vector(reference_vector, direction_vector)
+
+    # Compute final calculations
+    angle = numpy.arccos(reference_dot_direction / (reference_norm * direction_norm))
+    return angle
 
 
 mp_drawing = mp.solutions.drawing_utils
@@ -23,7 +47,8 @@ cap.set(cv2.CAP_PROP_FRAME_WIDTH, 720)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
 # Processes and displays video feed
-with mp_hands.Hands(max_num_hands=1, min_detection_confidence=0.95, min_tracking_confidence=0.1, static_image_mode=False) as hands:
+with mp_hands.Hands(max_num_hands=1, min_detection_confidence=0.95, min_tracking_confidence=0.1,
+                    static_image_mode=False) as hands:
     while cap.isOpened():
         success, image = cap.read()
         if not success:
